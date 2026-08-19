2acfe04534190adf41b5fb530c841a113b41b56c
SuperMKwhy <cubergankorn356@gmail.com>
2026-08-20 01:03:50 +0700

    initial commit from xcode

---
+++ fitQuest/fitQuest/fitQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitQuestApp.swift
+//  fitQuest
+//
+//  Created by Mangkorn Chunvijitra on 20/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct fitQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
